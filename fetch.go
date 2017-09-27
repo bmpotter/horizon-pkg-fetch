2baa8bb30525daa4ebeed05c96e55bafd5ffb158
michael dye <m-github@divisive.info>
2017-09-26 20:46:35 -0600

    fixed lint-reported warnings and reorganized file

--- fetch.go
+++ fetch.go
@@ -10,76 +10,76 @@ const (
 	maxFetchesBuffer      = 100
 )
 
-// FetchPool is a type that specifies the configuration of FetchTasks.
-type FetchPool struct {
+// Pool is a type that specifies the configuration of Tasks.
+type Pool struct {
 	DestinationDirectory string // DestinationDirectory needs to be an environment variable; it can't be changed during execution
 	HTTPClientProducer   func(domain string) *http.Client
-	FetchBuffer          chan *FetchTask
-	CancelationBuffer    chan *FetchCancelation
+	FetchBuffer          chan *Task
+	CancelationBuffer    chan *Cancelation
 }
 
-// EnqueueFetch takes a ??? TODO ???, generates a FetchTask and enqueues it for
+// EnqueueFetch takes a ??? TODO ???, generates a Task and enqueues it for
 // fetching.
-func (pool *FetchPool) EnqueueFetch(fetchTask *FetchTask) error {
+func (pool *Pool) EnqueueFetch(Task *Task) error {
 	return nil
 }
 
 // CancelFetch takes a cancelation instance and applies it to enqueued
-// FetchTasks.
-func (pool *FetchPool) CancelFetch(fetchCancelation *FetchCancelation) error {
+// Tasks.
+func (pool *Pool) CancelFetch(fetchCancelation *Cancelation) error {
 	return nil
 }
 
-// NewFetchPool configures and returns an instantiation of a FetchPool or an
+// NewPool configures and returns an instantiation of a Pool or an
 // error. The given HTTPClient producer function is expected to produce a
 // configured HTTPClient that can authenticate with the given domain).
-func NewFetchPool(destDir string, clientProducer func(domain string) *http.Client) (*FetchPool, error) {
+func NewPool(destDir string, clientProducer func(domain string) *http.Client) (*Pool, error) {
 
 	// bounded, async channels
-	pool := &FetchPool{
+	pool := &Pool{
 		DestinationDirectory: destDir,
 		HTTPClientProducer:   clientProducer,
-		FetchBuffer:          make(chan *FetchTask, maxFetchesBuffer),
-		CancelationBuffer:    make(chan *FetchCancelation, maxCancelationsBuffer),
+		FetchBuffer:          make(chan *Task, maxFetchesBuffer),
+		CancelationBuffer:    make(chan *Cancelation, maxCancelationsBuffer),
 	}
 
 	return pool, nil
 }
 
-// FetchQueueProcessor consumes enqueued FetchTasks if:
-// 0. Canceling a FetchTask (by setting CanceledOn timestamp), then dropping it
+// QueueProcessor consumes enqueued Tasks if:
+// 0. Canceling a Task (by setting CanceledOn timestamp), then dropping it
 // 1. Instantiating a new HTTPClient from the Pool's factory *just before* a
 // fetch attempt and then trying the fetch.
-// 2. Adding a new FetchTry to the FetchTask's FetchTryHistory w/ success or
+// 2. Adding a new Try to the Task's TryHistory w/ success or
 // failure.
 //
-// A FetchTask can be canceled before download (TODO: implement during-fetch
+// A Task can be canceled before download (TODO: implement during-fetch
 // cancelation too, later). That amounts to consuming a cancelation message on
-// the cancelation queue and then attaching it to a FetchTask
-type FetchQueueProcessor struct {
+// the cancelation queue and then attaching it to a Task
+type QueueProcessor struct {
 }
 
-// FetchTask is a job to track fetching a particular fetchable unit by a
+// Task is a job to track fetching a particular fetchable unit by a
 // FetchWorker.
-type FetchTask struct {
-	DestinationPath string // the identifier for the FetchTask
-	Cancelation     FetchCancelation
-	FetchTryHistory []FetchTry
+type Task struct {
+	DestinationPath string // the identifier for the Task
+	Cancelation     Cancelation
+	TryHistory      []Try
 	Pkg             *horizonpkg.Pkg
 }
 
-// FetchTry is a historical record of a fetch attempt, either successful or
+// Try is a historical record of a fetch attempt, either successful or
 // failed.
-type FetchTry struct {
+type Try struct {
 	FetchSuccess   bool
 	FetchMsg       string
 	FetchErr       error
 	FetchStartedAt int
 }
 
-// FetchCancelation indicates a cancelation of an enqueued FetchTask
-type FetchCancelation struct {
-	DestinationPath               string // the identifier for the cancelation that matches the FetchTask
+// Cancelation indicates a cancelation of an enqueued Task
+type Cancelation struct {
+	DestinationPath               string // the identifier for the cancelation that matches the Task
 	CanceledOn                    uint
 	CanceledBy                    string // loose description of the cancel request agent
 	CancelationRequestSubmittedAt uint   // timestamp indicating when cancelation was submitted; the pool will expire requests to cancel after a certain duration
