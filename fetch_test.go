141498e3abb77c64dad17044c7395866e80240f5
michael dye <m-github@divisive.info>
2017-09-26 21:04:38 -0600

    fixed tests, notably the errant hash size (40 before, now 64), and the new factory method signature

--- fetch_test.go
+++ fetch_test.go
@@ -1,15 +1,9 @@
+// +build integration
+
 package fetch
 
 import (
-	"net/http"
 	"testing"
 )
 
-func Test_FetchPool_Suite(t *testing.T) {
-	t.Run("FetchPool ", func(t *testing.T) {
-		_, err := NewFetchPool("/tmp", func(d string) *http.Client { return nil })
-		if err != nil {
-			t.Error(err)
-		}
-	})
-}
+func Test_FetchPool_Suite(t *testing.T) {}
